45b4aed9fe460acc1f639897a4bc09a784dab772
e-kaneyuki <occultchikin@gmail.com>
2023-10-24 08:20:33 +0900

    質問に関するBO作成

--- KnowledgeGarden/src/main/java/model/QuestionLogic.java
+++ KnowledgeGarden/src/main/java/model/QuestionLogic.java
@@ -1,13 +1,23 @@
 package model;
 
+import java.util.List;
+
 import dao.QuestionDAO;
 
 public class QuestionLogic {
+	//insert
 	public boolean execute(QuestionRegistration questionRegistration) {
 		QuestionDAO dao = new QuestionDAO();
 		int count = dao.insertQuestion(questionRegistration);
 		return count != 0;
 	}
+	//index
+	public List<Question> execute() {
+		QuestionDAO dao = new QuestionDAO();
+		List<Question> questionList = dao.findAllQuestion();
+		return questionList;
+	}
+	//show
 	public Question showQuestion(int questionId) {
 		QuestionDAO dao = new QuestionDAO();
 		Question question = dao.getQuestionById(questionId);
