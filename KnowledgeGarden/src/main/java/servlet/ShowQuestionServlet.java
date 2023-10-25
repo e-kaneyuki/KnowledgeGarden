5b3ded935dd220ed27ebe14f4d0957deee725dcc
e-kaneyuki <occultchikin@gmail.com>
2023-10-25 16:22:16 +0900

    質問詳細画面で回答が出力されるように変更。またデザインも一新。

--- KnowledgeGarden/src/main/java/servlet/ShowQuestionServlet.java
+++ KnowledgeGarden/src/main/java/servlet/ShowQuestionServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,6 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.Answer;
+import model.AnswerLogic;
 import model.Question;
 import model.QuestionLogic;
 
@@ -25,11 +28,21 @@ public class ShowQuestionServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// パラメータ "id" を取得
         int id = Integer.parseInt(request.getParameter("id"));
+        List<Answer> ansList = null;
         
         QuestionLogic queLog = new QuestionLogic();
         Question question = queLog.showQuestion(id);
-        
         request.setAttribute("question", question);
+        
+        AnswerLogic ansLog = new AnswerLogic();
+        ansList = ansLog.findAnswerById(id);
+        for(Answer ans: ansList) {
+        	System.out.println(ans.getContent());
+        }
+        if(ansList != null) {
+        	request.setAttribute("ansList", ansList);	
+        }
+        
         RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/showQuestion.jsp");
 		dispatcher.forward(request, response);
 	}
