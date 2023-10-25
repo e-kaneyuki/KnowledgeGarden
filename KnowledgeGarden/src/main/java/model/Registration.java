d385a32d3d3df2b8b687d23f05abb48beaa6b04d
e-kaneyuki <occultchikin@gmail.com>
2023-10-25 16:23:42 +0900

    Entityクラスとして、継承元を修正。

--- KnowledgeGarden/src/main/java/model/Registration.java
+++ KnowledgeGarden/src/main/java/model/Registration.java
@@ -1,6 +1,8 @@
 package model;
 
-public class Registration {
+import java.io.Serializable;
+
+public class Registration implements Serializable{
 	private int id;
 	private String name;
 	private String pass;
