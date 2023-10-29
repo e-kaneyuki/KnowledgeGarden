db08d904a11e42a503dd1ae5981e226cc5b4d3a0
e-kaneyuki <occultchikin@gmail.com>
2023-10-29 21:09:24 +0900

    不要なコメントを削除

--- KnowledgeGarden/src/main/java/servlet/SearchServlet.java
+++ KnowledgeGarden/src/main/java/servlet/SearchServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import dao.SearchDAO;
 import model.Question;
@@ -19,36 +20,22 @@ import model.Question;
 public class SearchServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SearchServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		request.setCharacterEncoding("utf-8");
-		//文字化け対策した上で変数セット
-		
-//		String questionTitle = request.getParameter("questionTitle");
-//        String questionContent = request.getParameter("questionContent");
-//        String questioner = request.getParameter("questioner");
-//        String answerContent = request.getParameter("answerContent");
-        
-//        SearchLogic logic = new SearchLogic();
-        
+ 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+ 		
+		HttpSession session = request.getSession();
+		String user = (String) session.getAttribute("userName");
+		if (user == null) {
+			String message = "ログインしてください。";
+			session.setAttribute("loginMessage", message);
+			response.sendRedirect("Login");
+			return;
+		}
+     
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/search.jsp");
 		dispatcher.forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		//文字化け対策した上で変数セット
@@ -56,14 +43,9 @@ public class SearchServlet extends HttpServlet {
 		SearchDAO dao = new SearchDAO();
         
         List<Question> logic = dao.searchQuestions(questionTitle);
-        for(Question logics: logic) {
-        	System.out.println(logics.getTitle());
-        }
+
         request.setAttribute("questions", logic);
         RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/indexQuestion.jsp");
 		dispatcher.forward(request, response);
 	}
-
-
-
 }
