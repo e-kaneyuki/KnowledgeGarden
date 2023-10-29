17f4eb908a863f2d6e48c9d6619114bf71a2d2bd
e-kaneyuki <occultchikin@gmail.com>
2023-10-30 08:11:16 +0900

    削除用BOを作成

--- KnowledgeGarden/src/main/java/model/QuestionLogic.java
+++ KnowledgeGarden/src/main/java/model/QuestionLogic.java
@@ -23,4 +23,11 @@ public class QuestionLogic {
 		Question question = dao.getQuestionById(questionId);
 		return question;
 	}
+	
+	//delete
+		public boolean deleteQuestion(int questionId) {
+			QuestionDAO dao = new QuestionDAO();
+			int count = dao.deleteQuestion(questionId);
+			return count != 0;
+		}
 }
